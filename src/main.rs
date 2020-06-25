8fd360cafb6b74e794149820822133cc6bc6ed29
Jillian <jillianjean98@gmail.com>
2020-06-25 12:16:25 -0400

    comment out ppm maker

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod vec3;
 fn main() -> io::Result<()> {
     let v = vec3::Vec3::new(0.0,1.0,0.0);
     println!("{}", v[1]);
+
+    // draw ppm image
     // const IMAGE_WIDTH:i32 = 256;
     // const IMAGE_HEIGHT:i32 = 256;
 
@@ -29,24 +31,3 @@ fn main() -> io::Result<()> {
     // io::stderr().write_all(b"\nDone.\n")?;
      Ok(())
 }
-
-// int main() {
-//     const int image_width = 256;
-//     const int image_height = 256;
-
-//     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-//     for (int j = image_height-1; j >= 0; --j) {
-//         for (int i = 0; i < image_width; ++i) {
-//             auto r = double(i) / (image_width-1);
-//             auto g = double(j) / (image_height-1);
-//             auto b = 0.25;
-
-//             int ir = static_cast<int>(255.999 * r);
-//             int ig = static_cast<int>(255.999 * g);
-//             int ib = static_cast<int>(255.999 * b);
-
-//             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-//         }
-//     }
-// }
